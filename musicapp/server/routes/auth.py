6a4b5883922158c664a008e196c7026370271119
Ravilovecode <rairavi9090@gmail.com>
2024-10-03 15:55:59 +0530

    all routes created and tested

--- musicapp/server/routes/auth.py
+++ musicapp/server/routes/auth.py
@@ -8,8 +8,10 @@ from pydantic_schemas.user_create import UserCreate
 from database import get_db
 from sqlalchemy.orm import Session
 
+from pydantic_schemas.user_login import UserLogin
+
 router = APIRouter()
-@router.post("/signup")
+@router.post("/signup", status_code=201)
 def signup_user(user: UserCreate, db: Session = Depends(get_db)):
    # Check if the user already exists in the database
    user_db = db.query(User).filter(User.email == user.email).first()
@@ -27,3 +29,23 @@ def signup_user(user: UserCreate, db: Session = Depends(get_db)):
    db.refresh(new_user)  # Refresh instance to reflect saved data (such as ID)
    
    return {"id": new_user.id, "name": new_user.name, "email": new_user.email}
+
+
+@router.post("/login", status_code=200)
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+   # Check if the user exists in the database
+   user_db = db.query(User).filter(User.email == user.email).first()
+
+   if not user_db:
+      raise HTTPException(status_code=400, detail="User does not exist")
+   
+      # Check if the password is correct
+   is_match = bcrypt.checkpw(user.password.encode('utf-8'), user_db.password)
+   
+   if not is_match:
+      raise HTTPException(status_code=400, detail="Incorrect password")
+   
+   return {"id": user_db.id, "name": user_db.name, "email": user_db.email}
+         
+   
+   pass
